d88f23983f1ac9257dc876248c4fc6db7abc2dba
raddevus <1radeutsch1@gmail.com>
2023-07-18 11:40:20 -0400

    Compiled all core competency data into one json file.

---
+++ CoreCo/Model/CoreCompetency.swift
@@ -0,0 +1,13 @@
+//
+//  CoreCompetency.swift
+//  CoreCo
+//
+//  Created by roger deutsch on 7/18/23.
+//
+
+import Foundation
+
+struct CoreCompetency: Codable: Identifiable{
+    let id: String
+    
+}
