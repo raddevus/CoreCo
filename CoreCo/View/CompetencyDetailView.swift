55b9093b0ddfc5a5683c99709fa3ef824c5c3961
raddevus <1radeutsch1@gmail.com>
2023-07-21 18:16:13 -0400

    Added code to handle Examples using Toggles. Added cleanup on UI so it's starting to look like a finished app now.

--- CoreCo/View/CompetencyDetailView.swift
+++ CoreCo/View/CompetencyDetailView.swift
@@ -9,19 +9,54 @@ import SwiftUI
 
 struct CompetencyDetailView: View {
     let coreCompetency: CoreCompetency
+    
     var body: some View {
-        Text(coreCompetency.description)
+        ScrollView{
+            VStack(){
+                Text(coreCompetency.title)
+                    .font(.title)
+                    .padding()
+                Text(coreCompetency.description)
+                    .font(.title2)
+                    .foregroundColor(.blue)
+                    .padding()
+                    .border(.black)
+                    .padding(EdgeInsets.init(top: CGFloat(0), leading: CGFloat(3), bottom: CGFloat(5), trailing: CGFloat(3)))
+                ForEach (coreCompetency.examples, id:\.self){ item in
+                    ExamplesToggleView(example: item)
+                    
+                }
+                Spacer()
+            }
+        }
     }
     
     init(competency: CoreCompetency){
         coreCompetency = competency
     }
+    
 }
 
+struct ExamplesToggleView: View {
+    
+    var example: String
+    @State private var exampleToggle = false
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Toggle(example, isOn: $exampleToggle)
+        }
+        .padding(.horizontal)
+        
+    }
+}
+
+let coreCompetency = CoreCompetency(id: "test-1", title: "test title", description: "This is a good long descript to give you an idea.", examples: ["yada","yada 2","yada 3"], group: "Self Management")
+
 struct CompetencyDetailView_Previews: PreviewProvider {
-    let coreCompetency = CoreCompetency(id: "test-1", title: "test title", description: "test descr", examples: [], group: "Self Management")
+  
     static var previews: some View {
-        // CompetencyDetailView(competency: coreCompetency)
-        Text("hello competency!")
+        CompetencyDetailView(competency: coreCompetency)
+        //Text("hello competency!")
     }
 }
